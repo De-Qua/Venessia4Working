927798263a11775847569587434ebdf1b8b69194
lucatastrophe <luca.deltorre9@gmail.com>
2020-03-17 17:26:48 +0100

    ridotte le colonne del shp

--- newGpd.py
+++ newGpd.py
@@ -11,15 +11,17 @@ import os
 # metodo per avere gli indici delle vie o della via trovata
 from mydifflib import get_close_matches_indexes
 
-from string import maketrans
+#from string import maketrans
 
 
 folder = os.getcwd()
+elstr_ponti = gpd.read_file(folder + "/pontiDivisi_completo" + "/pontiDivisi_completo.shp")
+elstr_point.columns
 streets_graph = gpd.read_file(folder + "/data" + "/EL_STR.shp")
 streets_names = gpd.read_file(folder + "/data" + "/TP_STR.shp")
 civico = gpd.read_file("/home/lucatastrophe/Desktop/venessia/Venessia4Working/data/Tema0301_ToponimiNumeriCivici/CIVICO.shp")
 
-trantab = {ord("0"):None,ord("1"):None,ord("2"):None,ord("3"):None,ord("4"):None,ord("5"):None,ord("6"):None,ord("7"):None,ord("8"):None,ord("9"):None,ord(","):None}
+#trantab = {ord("0"):None,ord("1"):None,ord("2"):None,ord("3"):None,ord("4"):None,ord("5"):None,ord("6"):None,ord("7"):None,ord("8"):None,ord("9"):None,ord(","):None}
 
 indirizzi = civico["INDIRIZZO"].to_list()
 sestiere = []
@@ -29,9 +31,10 @@ for ind in indirizzi:
     else:
         sestiere.append(ind.translate(trantab))
 
+new_ponti = gpd.GeoDataFrame(data = zip(elstr_ponti["geometry"],elstr_ponti["PONTE_TY"]), columns = ["geometry", "ponte"])
+new_ponti.to_file(folder + "/pontiDivisi_completo" + "/pontiDivisi_modificato.shp")
 
-
-total = gpd.GeoDataFrame(data = zip(civico["CIVICO_NUM"], sestiere, civico["DENOMINAZI"], civico["geometry"]),lunghezza, ponte, columns = ["NUMERO", "SESTIERE", "TOPONIMO", "geometry", "lunghezza", "ponte"])
+#total = gpd.GeoDataFrame(data = zip(civico["CIVICO_NUM"], sestiere, civico["DENOMINAZI"], civico["geometry"]),lunghezza, ponte, columns = ["NUMERO", "SESTIERE", "TOPONIMO", "geometry", "lunghezza", "ponte"])
 #venezia_ind  = total["SESTIERE"].str.contains( "DORSODURO|CANNAREGIO|SAN POLO|^SAN MARCO|SANTA CROCE|CASTELLO") #aggiungere isole
 #total.index = venezia_ind
 #venezia = total.loc[True]
